cb2d48a2770845ce994a9122c41252a2695c665b
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-08-07 22:31:01 +0300

    hw-06 done

--- js/task-10.js
+++ js/task-10.js
@@ -4,35 +4,32 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 const elements = {
-  quantityBox: document.querySelector("input"),
+  setQuantity: document.querySelector("input"),
   createBtn: document.querySelector("button[data-create]"),
   destroyBtn: document.querySelector("button[data-destroy]"),
   boxes: document.querySelector("#boxes"),
 };
-let amount = 5;
-console.log(elements.quantityBox.value);
+
 elements.createBtn.addEventListener("click", handlerClick);
 elements.destroyBtn.addEventListener("click", destroyBoxes);
 
 function destroyBoxes() {
   elements.boxes.innerHTML = "";
 }
-function handlerClick(amount) {
+function handlerClick() {
+  const amount = elements.setQuantity.value;
   createBoxes(amount);
 }
 
 function createBoxes(amount) {
   let arr = [];
-  for (let i = 0; i <= amount; i += 1) {
+  for (let i = 0; i < amount; i += 1) {
     const size = 30 + i * 10;
     const color = getRandomHexColor();
     arr.push(
-      `<div style = 'width: ${size}px; height: ${size}px; color = ${color}'></div>`
+      `<div style = 'width: ${size}px; height: ${size}px; background-color: ${color}'></div>`
     );
   }
-  console.log(arr);
   let markup = arr.map((el) => el).join("");
-  console.log(markup);
-  // console.log(elements.quantityBox.value);
-  elements.boxes.innertHTML(markup);
+  elements.boxes.innerHTML = markup;
 }
