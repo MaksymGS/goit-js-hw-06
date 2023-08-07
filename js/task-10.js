931af893140e6ce9c27f9c67c3569e63817b3ef2
MaksymGS <maximgsua@gmail.com>
2023-08-07 18:29:16 +0300

    Somthing task10

--- js/task-10.js
+++ js/task-10.js
@@ -4,18 +4,35 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 const elements = {
-  quantityBox: document.querySelector('#controls'),
-  createBtn: document.querySelector('button[data-create]'),
-  destroyBtn: document.querySelector('button[data-destroy]'),
-  boxes: document.querySelector('#boxes')
-}
-elements.createBtn.addEventListener('click', createBoxes);
-elements.destroyBtn.addEventListener('click', destroyBoxes);
+  quantityBox: document.querySelector("input"),
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+let amount = 5;
+console.log(elements.quantityBox.value);
+elements.createBtn.addEventListener("click", handlerClick);
+elements.destroyBtn.addEventListener("click", destroyBoxes);
 
 function destroyBoxes() {
-  elements.boxes.innerHTML = ''
+  elements.boxes.innerHTML = "";
+}
+function handlerClick(amount) {
+  createBoxes(amount);
 }
 
-function createBoxes() {
-  console.log(elements.quantityBox.value);
+function createBoxes(amount) {
+  let arr = [];
+  for (let i = 0; i <= amount; i += 1) {
+    const size = 30 + i * 10;
+    const color = getRandomHexColor();
+    arr.push(
+      `<div style = 'width: ${size}px; height: ${size}px; color = ${color}'></div>`
+    );
+  }
+  console.log(arr);
+  let markup = arr.map((el) => el).join("");
+  console.log(markup);
+  // console.log(elements.quantityBox.value);
+  elements.boxes.innertHTML(markup);
 }
